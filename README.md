b42ae4a4dfee41f3c17c0c73147e74cf1fbe22ee
Wei Shen <shenwei356@gmail.com>
2018-11-21 20:35:43 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TODO list
+
+Just a toy for learning web development.
---
+++ docs/js/todo.js
@@ -0,0 +1,51 @@
+ 
+function newBtnClose(text, parent){
+    var btn = $("<button/>").addClass("pull-right close ml-3");
+    btn.html(text);
+    btn.attr("id", "btn-close");
+    btn.attr("title", "Close the task");
+    btn.on("click", function () {
+        var div = $(this).parents(parent);
+        div.remove();
+    });
+    return btn
+}
+
+function newBtnDone(text, parent){
+    var btn = $("<button/>").addClass("pull-right close ml-3");
+    btn.html(text);
+    btn.attr("id", "btn-done");
+    btn.attr("title", "Mark as finished");
+    btn.on("click", function () {
+        var div = $(this).parents(parent);
+        div.children("#btn-done").remove();
+        div.appendTo($("#list-done"));
+    });
+    return btn
+}
+
+$("li", "#list-todo").each(function (i, elm) {
+    newBtnDone("&#x2713;", "li").appendTo($(elm));
+    newBtnClose("&#215;", "li").appendTo($(elm));
+});
+
+$("li", "#list-done").each(function (i, elm) {
+    newBtnClose("&#215;", "li").appendTo($(elm));
+});
+
+$("#btn-add-item").on("click", function() {
+    var lstTodoItem = $("<li/>");
+    lstTodoItem.addClass("nav-link")
+
+    var inputValue = $("#ipt-item").val();
+    if (inputValue === '') {
+        alert("Please enter task name!");
+    } else {
+        lstTodoItem.append($("<span>" + inputValue + "</span>"));
+        lstTodoItem.appendTo($("#list-todo"));
+    }
+    $("#ipt-item").val("");
+
+    newBtnDone("&#x2713;", "li").appendTo(lstTodoItem);
+    newBtnClose("&#215;", "li").appendTo(lstTodoItem);
+})
